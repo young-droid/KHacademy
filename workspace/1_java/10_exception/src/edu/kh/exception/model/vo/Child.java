d7c2ba9399901369c19823f662a466706d470a51
young-droid <young.yj.cho@gmail.com>
2024-11-11 17:49:14 +0900

    Exception small changes saved

--- workspace/1_java/10_exception/src/edu/kh/exception/model/vo/Child.java
+++ workspace/1_java/10_exception/src/edu/kh/exception/model/vo/Child.java
@@ -4,21 +4,21 @@ import java.io.FileNotFoundException;
 
 public class Child extends Parent {
 
-	@Override 
+	@Override
 //	public void method() throws IOException {
 	public void method() throws FileNotFoundException {
-		
+
 		System.out.println("오버라이딩된 자식 메소드");
-		
+
 		// 오버라이딩 시
 		// 예외는 같거나 더 좁은 범위
 		// * 좁은 범위 == 구체적인 예외
-		
+
 		// FileNotFoundException 은
-		// IOException의 자식 예외이므로 오버라이딩 가능! 
-		
+		// IOException의 자식 예외이므로 오버라이딩 가능!
+
 		// Exception(모든 예외의 부모)은
 		// IOException의 부모 예외이므로 오버라이딩 불가
 	}
-	
+
 }
