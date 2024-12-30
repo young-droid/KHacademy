bb83775c70d5e85c68b8fdc89e59bf4b1c6a83aa
조영주 <young.yj.cho@gmail.com>
2024-12-30 17:06:19 +0900

    git 캐시 삭제

--- workspace/1_java/00_javaBasic/src/edu/kh/basic/HarimRun.java
+++
@@ -1,9 +0,0 @@
-package edu.kh.basic;
-
-public class HarimRun {
-	public static void main(String[] args) {
-		Harimpractice ex = new Harimpractice();
-		
-		ex.ex1();
-	}
-}
--- workspace/3_JDBC/TestProject/src/edu/kh/test/Homework.java
+++
@@ -1,8 +0,0 @@
-package edu.kh.test;
-
-public class Homework {
-	public static void main(String[] args) {
-		System.out.println("숙제 완성!");
-		System.out.println("학원에서 공부한 내용");
-	}
-}
